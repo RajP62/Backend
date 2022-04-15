f23bef14d93562e1ea1c6550b8db09af42947d34
Your Name <you@example.com>
2022-04-15 21:28:00 +0530

    day-1

---
+++ simple calculator/index.js
@@ -0,0 +1,11 @@
+const add=require('./add')
+console.log(add(2,5))
+
+const sub=require('./subtract')
+console.log(sub(10,5))
+
+const multiply = require('./multiply')
+console.log(multiply(2,3))
+
+const divide=require('./divide')
+console.log(divide(10,5))
